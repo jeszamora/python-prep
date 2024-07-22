7416ea3b7807a3aed07c5326e3a2327607425b92
Jesse Zamora <jz42124@gmail.com>
2024-07-21 21:35:58 -0700

    Add input/output exercises

---
+++ input_output/in_out_exercises/age.py
@@ -0,0 +1,11 @@
+age = input('How old are you? ')
+
+future_ages = f'''
+You are {age} years old.
+In 10 years, you will be {int(age) + 10} years old.
+In 20 years, you will be {int(age) + 20} years old.
+In 30 years, you will be {int(age) + 30} years old.
+In 40 years, you will be {int(age) + 40} years old.
+'''
+
+print(future_ages)
